f93461fc4553fd5d6f677eae9bc0c819e191c5b1
Tomás <tomasarreguib@gmail.com>
2023-11-30 11:16:42 -0300

    feat(domain): added base class for value objects

---
+++ src/1.domain/profile/ValueObjects/base.vo.ts
@@ -0,0 +1,16 @@
+type BaseValueObjectProps = {
+  [index: string]: any;
+}
+
+export abstract class BaseValueObject<T extends BaseValueObjectProps> {
+  private readonly props: T;
+
+  constructor(props: T) {
+    this.props = props;
+  }
+
+  equals(value: T): boolean {
+    if (!(value instanceof BaseValueObject<T>)) return false;
+    return JSON.stringify(value) == JSON.stringify(this.props)
+  }
+}
