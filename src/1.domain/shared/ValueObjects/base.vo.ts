57d95a05549b7e9784cc99091c55d6a87a5575c6
Tomás <tomasarreguib@gmail.com>
2023-11-30 16:09:27 -0300

    feat(domain): added some extra validations in Base VO Added a reference to one article where talks about the static factory method to check later

--- src/1.domain/shared/ValueObjects/base.vo.ts
+++ src/1.domain/shared/ValueObjects/base.vo.ts
@@ -2,6 +2,12 @@ type BaseValueObjectProps = {
   [index: string]: any;
 }
 
+/*
+
+  Implemented BaseValueObject following this article:
+  https://khalilstemmler.com/articles/typescript-value-object/#Value-Object-class
+
+*/
 export abstract class BaseValueObject<T extends BaseValueObjectProps> {
   private readonly props: T;
 
@@ -13,6 +19,9 @@ export abstract class BaseValueObject<T extends BaseValueObjectProps> {
 
   equal(value: T): boolean {
     if (!(value instanceof BaseValueObject<T>)) return false;
+    if (value === null || value === undefined) return false
+    if (value.props === undefined) return false;
+
     return JSON.stringify(value) == JSON.stringify(this.props)
   }
 }
