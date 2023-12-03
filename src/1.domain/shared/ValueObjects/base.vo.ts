80f376d593f2edecd9f67f2267c07d6449304a2e
Tomás <tomasarreguib@gmail.com>
2023-12-03 18:25:43 -0300

    feat(domain): added getter prpos

--- src/1.domain/shared/ValueObjects/base.vo.ts
+++ src/1.domain/shared/ValueObjects/base.vo.ts
@@ -17,11 +17,15 @@ export abstract class BaseValueObject<T extends BaseValueObjectProps> {
 
   abstract isValid(props: T): boolean;
 
+  get props(): T {
+    return this._props;
+  }
+
   equal(value: T): boolean {
     if (!(value instanceof BaseValueObject<T>)) return false;
     if (value === null || value === undefined) return false
-    if (value.props === undefined) return false;
+    if (value._props === undefined) return false;
 
-    return JSON.stringify(value) == JSON.stringify(this.props)
+    return JSON.stringify(value) == JSON.stringify(this._props)
   }
 }
